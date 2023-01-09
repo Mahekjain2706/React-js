df5a5837f07b827325d892ae004d8e1c461966e1
Mahek jain <lci2021028@iiitl.ac.in>
2023-01-10 00:54:57 +0530

    Change 4

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 // different color palid dark mode , not count empty string
-
+// spaces lentence,layout shift,if text empty button not work.
 // Routers Dom 
 
 function App() {
@@ -29,11 +28,23 @@ function App() {
         setAlert(null);
        },2000);
   }
+
+  // const removeBodyClasses=()=>{
+  //     document.body.classList.remove('bg-light');
+  //     document.body.classList.remove('bg-dark');
+  //     document.body.classList.remove('bg-warning');
+  //     document.body.classList.remove('bg-success');
+  //     document.body.classList.remove('bg-primary');
+  //     document.body.classList.remove('bg-danger');
+  // }
   
-  const toggleMode = ()=>{
+  const toggleMode = (cls)=>{
+    // removeBodyClasses();
+    // console.log(cls);
+    // document.body.classList.add('bg-'+cls)
     if(mode==='light'){
       setMode('dark');
-      document.body.style.backgroundColor = 'grey';
+      document.body.style.backgroundColor = '#112b45';
       showAlert("Enable Dark Mode","success");
       document.title = 'TextUtils - Dark Mode';
       // setInterval(() => {
@@ -50,19 +61,17 @@ function App() {
     }
   }
   return (
-    <>
-    {/* <Router> */}
+    <Router>
       <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-      {/* <Routes>
-          <Route path='/' element={<TextForm heading="Enter The text to analyze below " mode={mode} toggleMode={toggleMode} showAlert={showAlert} />}/>
-          <Route path='/about' element={<About />}/>
-      </Routes> */}
-        <TextForm heading="Enter The text to analyze below " mode={mode} toggleMode={toggleMode} showAlert={showAlert} />
+      <Routes>
+          <Route path='/' element={<TextForm heading="Enter The text to analyze below " mode={mode} showAlert={showAlert} />}/>
+          <Route path='/about' element={<About mode={mode}/>}/>
+      </Routes>
       </div>
-    {/* </Router> */}
-    </>
+    </Router>
+
   ); 
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,9 +20,10 @@ export default function TextForm(props) {
         props.showAlert("Clear Text!","danger");
     }
     const handleCopy = () =>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        // var text = document.getElementById("myBox");
+        // text.select();  // no need
+        navigator.clipboard.writeText(text);
+        // document.getSelection().removeAllRanges();
         props.showAlert("Copied Text!","warning");
     }
     const handleExtraSpaces = () =>{
@@ -63,26 +64,25 @@ export default function TextForm(props) {
         <div className="container" style={{color: props.mode==='dark'?'white':'black'}} >
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='light'?'black':'white'}} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" style={{backgroundColor: props.mode==='light'?'white':'#112b45', color: props.mode==='light'?'black':'white'}} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleDownClick} > Convert To Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick} > Clear Text </button>
-            <button onClick={speak} className="btn btn-primary mx-2 my-2">Speak</button>
-            <button onClick={handleCopy} className="btn btn-primary mx-2 my-2">Copy Text</button>
-            <button onClick={handleExtraSpaces} className="btn btn-primary mx-2 my-2">Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert To Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleDownClick} > Convert To Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick} > Clear Text </button>
+            <button disabled={text.length===0} onClick={speak} className="btn btn-primary mx-1 my-1">Speak</button>
+            <button disabled={text.length===0} onClick={handleCopy} className="btn btn-primary mx-1 my-1">Copy Text</button>
+            <button disabled={text.length===0} onClick={handleExtraSpaces} className="btn btn-primary mx-1 my-1">Remove Extra Spaces</button>
 
 
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p>{text.split(".").length-1} senetnces</p>
 
-            <p>{0.008 * (text.split(' ').length)} Minutes to read.</p>
+            <p>{0.008 * (text.split(" ").filter((element)=>{return element.length!==0}).length)} Minutes to read.</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Eneter text to preview here ."}</p>
-
+            <p>{text.length>0?text:"Nothing to Preview !!"}</p>
         </div>
         </>
     )
